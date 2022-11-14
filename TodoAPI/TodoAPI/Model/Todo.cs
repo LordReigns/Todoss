1062982cf1d0e45961c9faa7ad7eef927e4a392d
LordReigns <nisanabeywickrama@gmail.com>
2022-11-14 14:27:00 +0530

    add models using db-first approach

---
+++ TodoAPI/TodoAPI/Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoAPI.Model;
+
+public partial class Todo
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool? IsCompleted { get; set; }
+}
---
+++ TodoAPI/TodoAPI/Model/TodoDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Model;
+
+public partial class TodoDbContext : DbContext
+{
+    public TodoDbContext()
+    {
+    }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.\\;Database=TodoDB;TrustServerCertificate=True;Integrated Security=SSPI;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("todo");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
+            entity.Property(e => e.IsCompleted).HasColumnName("isCompleted");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
